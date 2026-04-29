154652754866ae1345410fd45e94ded4bd4ee720
ippstuff436 <ippstuff4@gmail.com>
2026-04-28 20:16:40 -0500

    images

--- OnThisEarth_McMathDouglas.js
+++ OnThisEarth_McMathDouglas.js
@@ -198,35 +198,44 @@ ledger.innerHTML =
         <div style='display:flex;height:120px; overflow-x: hidden;overflow-y:hidden;'>
         <img src='https://file.garden/aKoRqbLZKBsHizSA/MacArthur.jpg' style='width:90px;height:120px;'>
         <p style='display: block;margin-top: 4px;margin-bottom: 4px;margin-left: 2px;margin-right: 2px;'>
-        <span style="display: inline-block; font-size: 1.5em; font-weight: bold; margin: 0.5em 0;">General MacArthur</span>
+        <span style="display: inline-block; font-size: 1.5em; font-weight: bold; margin: 0.5em 0;">The Conspiracy</span>
         <br>
-        ${textArray.otxt.MacArthurCharacterDesc}
+        <p>The Conspiracy</p>
         </div>
         <br>
         <div style='display:flex;height:120px;overflow-x: hidden;overflow-y:hidden;'>
         <img src='https://file.garden/aKoRqbLZKBsHizSA/Hoover.jpg' style='width:90px;height:120px;'>
         <p style='display: block;margin-top: 4px;margin-bottom: 4px;margin-left: 2px;margin-right: 2px;'>
-        <span style="display: inline-block; font-size: 1.5em; font-weight: bold; margin: 0.5em 0;">J. Edgar Hoover</span>
+        <span style="display: inline-block; font-size: 1.5em; font-weight: bold; margin: 0.5em 0;">The Oligarchy</span>
         <br>
-        ${textArray.otxt.HooverCharacterDesc}
+        <p>The Oligarchy</p>
         </div>
         <br>
         <br>
         <div style='display:flex;height:120px; overflow-x: hidden;overflow-y:hidden;'>
         <img src='https://file.garden/aKoRqbLZKBsHizSA/Rockefeller.jpeg' style='width:90px;height:120px;'>
         <p style='display: block;margin-top: 4px;margin-bottom: 4px;margin-left: 2px;margin-right: 2px;'>
-        <span style="display: inline-block; font-size: 1.5em; font-weight: bold; margin: 0.5em 0;">David Rockefeller</span>
+        <span style="display: inline-block; font-size: 1.5em; font-weight: bold; margin: 0.5em 0;">The Republic</span>
         <br>
-        ${textArray.otxt.RockefellerCharacterDesc}
+        <p>The Republic</p>
         </div>
         <br>
         <br>
         <div style='display:flex;height:120px;overflow-x: hidden; overflow-y:hidden;'>
         <img src='https://file.garden/aKoRqbLZKBsHizSA/Howard.jpg' style='width:90px;height:120px;'>
         <p style='display: block;margin-top: 4px;margin-bottom: 4px;margin-left: 2px;margin-right: 2px;'>
-        <span style="display: inline-block; font-size: 1.5em; font-weight: bold; margin: 0.5em 0;">Howard Hughes</span>
+        <span style="display: inline-block; font-size: 1.5em; font-weight: bold; margin: 0.5em 0;">The Executive</span>
         <br>
-        ${textArray.otxt.HughesCharacterDesc}
+        <p>The Executive</p>
+        </div>
+        <br>
+        <br>
+        <div style='display:flex;height:120px;overflow-x: hidden; overflow-y:hidden;'>
+        <img src='https://file.garden/aKoRqbLZKBsHizSA/Howard.jpg' style='width:90px;height:120px;'>
+        <p style='display: block;margin-top: 4px;margin-bottom: 4px;margin-left: 2px;margin-right: 2px;'>
+        <span style="display: inline-block; font-size: 1.5em; font-weight: bold; margin: 0.5em 0;">The Workers</span>
+        <br>
+        <p>The Workers</p>
         </div>
     </div>
 </div>
